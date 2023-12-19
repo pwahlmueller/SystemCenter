545895beb4fed321896b64523bb7ee334fe1e7a4
Patrick Wahlmueller <patrick@wahlmueller.it>
2023-12-19 11:34:25 +0100

    Get-Workflowhistory

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ SystemCenter things
 
 * [Troubleshooting Workflows in Service Manager](https://techcommunity.microsoft.com/t5/system-center-blog/troubleshooting-workflows-in-service-manager/bc-p/4010666#M3775)
 * [Troubleshooting Workflow Performance and Delays](https://techcommunity.microsoft.com/t5/system-center-blog/troubleshooting-workflow-performance-and-delays/ba-p/347510)
+* [See Workflkow history](https://social.technet.microsoft.com/wiki/contents/articles/53441.scsm-sql-quickly-view-existing-workflow-history-of-an-incident.aspx)
---
+++ ServiceManager/Get-Workflowhistory.sql
@@ -0,0 +1,17 @@
+﻿
+USE ServiceManager;
+SELECT TOP 1000 LT.[LTValue] AS 'Workflow Name' ,
+Wflw.[RuleName] AS 'Workflow InternalName' ,
+JobDetail.[ErrorCode] JobDetail.[ErrorMessage] ,
+JobDetail.[LastModified] ,
+JobDetail.[Output] AS 'Workflow Actions Log (XML)' ,
+JobDetail.[TimeStarted] ,
+JobDetail.[TimeScheduled] ,
+JobDetail.[TimeFinished]
+FROM [dbo].[WindowsWorkflowTaskJobStatus] WinWorkflow (nolock)
+JOIN [dbo].[Rules] Wflw (nolock) ON Wflw.RuleId = WinWorkflow.[RuleId]
+JOIN [dbo].[LocalizedText] LT (nolock) ON Wflw.[RuleId] = LT.[MPElementId]
+AND LT.LTStringType=1 AND LT.LanguageCode = 'ENU'
+JOIN [dbo].[JobStatus] JobDetail (nolock) ON JobDetail.[BatchId] = WinWorkflow.[BatchId]
+JOIN [dbo].[MT_System$WorkItem$Incident] IR (nolock) ON WinWorkflow.[BaseManagedEntityId] = IR.[BaseManagedEntityId]
+--WHERE WinWorkflow.[BaseManagedEntityId] LIKE 'F2647B28-71D7-486F-FBC0-80B498A8F9D0' WHERE IR.[Id_9A505725_E2F2_447F_271B_9B9F4F0D190C] LIKE 'IR708644'
