5f0b42223618498b658c07f9efa67ea5bc7f327e
Patrick Wahlmueller <patrick@wahlmueller.it>
2023-12-19 11:46:26 +0100

    Duration added

--- ServiceManager/Get-SCSMJobStatus.sql
+++ ServiceManager/Get-SCSMJobStatus.sql
@@ -1,5 +1,13 @@
-﻿
+﻿-- Get long running SCSM Workflows
 
-select SubmittedBy, RunningAs, Status, convert(xml,Output), ErrorCode,ErrorMessage, TimeScheduled, TimeStarted, TimeFinished
-from JobStatus
-order by TimeFinished desc
+select SubmittedBy, RunningAs, Status, convert(xml,Output), ErrorCode,ErrorMessage, TimeScheduled, TimeStarted, TimeFinished,
+DATEDIFF(minute, TimeScheduled,TimeFinished) AS ScheduleToFinish,
+DATEDIFF(minute, TimeScheduled,TimeStarted) AS ScheduleToStart,
+DATEDIFF(minute, TimeStarted,TimeFinished) AS Starttofinish
+
+
+    from JobStatus with (nolock)
+	where TimeStarted > '2023-12-18 14:00:00'
+	and (DATEDIFF(minute, TimeScheduled,TimeFinished)) > 10
+
+    order by TimeFinished desc
