4cede32c04b549e69ba4088eb14238e078203f10
Patrick Wahlmueller <patrick@wahlmueller.it>
2023-12-19 11:54:17 +0100

    Comments

--- ServiceManager/Update-Workflow.sql
+++ ServiceManager/Update-Workflow.sql
@@ -2,7 +2,7 @@
 -- https://techcommunity.microsoft.com/t5/system-center-blog/troubleshooting-workflow-performance-and-delays/ba-p/347510
 
 Declare @maxtransactionid int, @RuleID uniqueidentifier
-Set @RuleId = '0F3D5133-E5AD-D9E0-01CD-8C5A3F122533'
+Set @RuleId = '' -- use for single ruleid
 set @MaxTransactionid = (select MAX(Entitytransactionlogid) from entitytransactionlog)
 
 update cmdbinstancesubscriptionstate
@@ -11,7 +11,7 @@ where ruleid  in (
 
 select distinct r.ruleid from rules R
 LEFT OUTER JOIN LocalizedText AS LT On R.RuleId = LT.MPElementId 
-where ltvalue like '%Incidents_Prioriy%'
+where ltvalue like '%<RuleName>%'  -- use for using Rule Name
 AND LT.LTStringType = 1 
 and lt.languagecode = 'ENU'
 and r.ruleenabled <> 0
