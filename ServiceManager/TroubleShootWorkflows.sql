7f7e2df1faed49de35cfa89a535319700d661928
Patrick Wahlmueller <patrick@wahlmueller.it>
2023-12-19 11:13:22 +0100

    join insted of select

--- ServiceManager/TroubleShootWorkflows.sql
+++ ServiceManager/TroubleShootWorkflows.sql
@@ -5,9 +5,12 @@ SET @MaxState = ( SELECT MAX(EntityTransactionLogId) FROM EntityChangeLog WITH(N
 SET @MaxStateDate = ( SELECT TimeAdded FROM EntityTransactionLog WhERE EntityTransactionLogId = @MaxState )
 
 SELECT  LT.LTValue AS 'Display Name', 
+		r.RuleId as 'RuleId',
+		r.ManagementPackId as RuleMPID,
         S.State AS 'Current Workflow Watermark', 
         @MaxState AS 'Current Transaction Log Watermark', 
-        DATEDIFF(mi,(SELECT TimeAdded FROM EntityTransactionLog WITH(NOLOCK) WHERE EntityTransactionLogId = S.State), @MaxStateDate) AS 'Minutes Behind', 
+        DATEDIFF(mi,ETL.timeadded, @MaxStateDate) AS 'Minutes Behind', 
+		etl.TimeAdded,
         S. EventCount, S. LastNonZeroEventCount, R.RuleName AS 'MP Rule Name', 
         MT.TypeName AS 'Source Class Name', 
         S.LastModified AS 'Rule Last Modified', 
@@ -16,12 +19,18 @@ FROM CmdbInstanceSubscriptionState AS S WITH(NOLOCK)
 LEFT OUTER JOIN Rules AS R ON S.RuleId = R.RuleId 
 LEFT OUTER JOIN ManagedType AS MT ON S.TypeId = MT.ManagedTypeId 
 LEFT OUTER JOIN LocalizedText AS LT On R.RuleId = LT.MPElementId 
+left outer join EntityTransactionLog as ETL on etl.EntityTransactionLogId = s.State
+
 
 WHERE S.State <= @MaxState - @Delta 
 AND R.RuleEnabled <> 0 
 AND LT.LTStringType = 1 
 AND LT.LanguageCode = @Language 
 AND S.IsPeriodicQueryEvent = 0 
+--and lt.LTValue like '%'  -- Bezeichnung der Rule
+--and DATEDIFF(mi,ETL.timeadded, @MaxStateDate) > 20
+
+--where r.ruleid = '0F3D5133-E5AD-D9E0-01CD-8C5A3F122533'
 --AND LT.LTValue LIKE '%Test%' -- for workflowname
 
 ORDER BY S.State Asc 
